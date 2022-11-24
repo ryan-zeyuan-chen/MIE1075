f38ddc2a2f3774990c4dcbd5569d6d8dfa1ccfee
Ryan Zeyuan Chen <zeyuan.chen@mail.utoronto.ca>
2022-11-23 22:06:50 -0500

    added predict_depth function

---
+++ depth/predict.py
@@ -0,0 +1,81 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import sys
+import numpy as np
+import PIL.Image as pil
+
+import torch
+from torchvision import transforms, datasets
+
+import networks
+from layers import disp_to_depth
+from utils import download_model_if_doesnt_exist
+from evaluate_depth import STEREO_SCALE_FACTOR
+
+def open_image(image_path):
+    input_image = pil.open(image_path).convert('RGB')
+    return input_image
+
+def predict_depth(input_image):
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    download_model_if_doesnt_exist("mono+stereo_1024x320")
+    model_path = os.path.join("models", "mono+stereo_1024x320")
+    print("-> Loading model from ", model_path)
+    encoder_path = os.path.join(model_path, "encoder.pth")
+    depth_decoder_path = os.path.join(model_path, "depth.pth")
+
+    # LOADING PRETRAINED MODEL
+    print("   Loading pretrained encoder")
+    encoder = networks.ResnetEncoder(18, False)
+    loaded_dict_enc = torch.load(encoder_path, map_location=device)
+
+    # extract the height and width of image that this model was trained with
+    feed_height = loaded_dict_enc['height']
+    feed_width = loaded_dict_enc['width']
+    filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
+    encoder.load_state_dict(filtered_dict_enc)
+    encoder.to(device)
+    encoder.eval()
+
+    print("   Loading pretrained decoder")
+    depth_decoder = networks.DepthDecoder(
+        num_ch_enc=encoder.num_ch_enc, scales=range(4))
+
+    loaded_dict = torch.load(depth_decoder_path, map_location=device)
+    depth_decoder.load_state_dict(loaded_dict)
+
+    depth_decoder.to(device)
+    depth_decoder.eval()
+
+    # PREDICTING ON IMAGE
+    with torch.no_grad():
+
+        # Load image and preprocess
+        original_width, original_height = input_image.size
+        input_image = input_image.resize((feed_width, feed_height), pil.LANCZOS)
+        input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+
+        # PREDICTION
+        input_image = input_image.to(device)
+        features = encoder(input_image)
+        outputs = depth_decoder(features)
+
+        disp = outputs[("disp", 0)]
+        disp_resized = torch.nn.functional.interpolate(
+            disp, (original_height, original_width), mode="bilinear", align_corners=False)
+
+        scaled_disp, depth = disp_to_depth(disp, 0.1, 100)
+        metric_depth = np.squeeze(STEREO_SCALE_FACTOR * depth.cpu().numpy())
+
+    print('-> Done!')
+    return metric_depth
+
+if __name__ == '__main__':
+    input_image = open_image('assets/frame360.jpg')
+    depth = predict_depth(input_image)
+    print(depth)
